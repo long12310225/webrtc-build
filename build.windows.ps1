e0f1ea3444f34c76405a5b0777c78cf72534592b
375102274@qq.com <375102274@qq.com>
2023-12-26 09:15:29 +0800

    x

--- build.windows.ps1
+++ build.windows.ps1
@@ -3,9 +3,9 @@ $ErrorActionPreference = 'Stop'
 $SCRIPT_DIR = (Resolve-Path ".").Path
 
 Push-Location $SCRIPT_DIR
-  # リポジトリの下に置きたいが、GitHub Actions の D:\ の容量が少なくてビルド出来ない問題があるので
-  # このパスにソースを配置する。
-  # また、WebRTC のビルドしたファイルは同じドライブに無いといけないっぽいのでこちらも設定する。
-  python3 run.py build windows --source-dir 'C:\webrtc' --build-dir 'C:\webrtc-build'
-  python3 run.py package windows --source-dir 'C:\webrtc' --build-dir 'C:\webrtc-build'
+# リポジトリの下に置きたいが、GitHub Actions の D:\ の容量が少なくてビルド出来ない問題があるので
+# このパスにソースを配置する。
+# また、WebRTC のビルドしたファイルは同じドライブに無いといけないっぽいのでこちらも設定する。
+python3 run.py build windows --source-dir 'E:\webrtc' --build-dir 'E:\webrtc-build'
+python3 run.py package windows --source-dir 'E:\webrtc' --build-dir 'E:\webrtc-build'
 Pop-Location
