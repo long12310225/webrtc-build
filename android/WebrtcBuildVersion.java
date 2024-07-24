06443f611130c046580a26b4e24cab324d5d32bd
李耀聪 <375102274@qq.com>
2024-07-24 14:13:12 +0800

    x

---
+++ android/WebrtcBuildVersion.java
@@ -0,0 +1,7 @@
+package org.webrtc;
+public interface WebrtcBuildVersion {
+    public static final String webrtc_branch = "M94";
+    public static final String webrtc_commit = "3";
+    public static final String webrtc_revision = "b83487f08ff836437715b488f73416215e5570dd";
+    public static final String maint_version = "0";
+}
